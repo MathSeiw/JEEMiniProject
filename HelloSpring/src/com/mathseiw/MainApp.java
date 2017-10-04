49c06418f1de10d5272475a5f76a1b41f5ea1b82
Mathieu Seiwert <seiwert.mathieu@gmail.com>
2017-10-04 11:56:10 +0200

    publish and handle Custom Spring Events

--- HelloSpring/src/com/mathseiw/MainApp.java
+++ HelloSpring/src/com/mathseiw/MainApp.java
@@ -1,25 +1,17 @@
 package com.mathseiw;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
       ConfigurableApplicationContext context = 
          new ClassPathXmlApplicationContext("Beans.xml");
-
-      // Let us raise a start event.
-      context.start();
       
-      ApplicationContext ctx = 
-              new AnnotationConfigApplicationContext(HelloWorldConfig.class);
-        
-           HelloWorld helloWorld = ctx.getBean(HelloWorld.class);
-      helloWorld.getMessage();
-
-      // Let us raise a stop event.
-      context.stop();
+      CustomEventPublisher cvp = 
+         (CustomEventPublisher) context.getBean("customEventPublisher");
+      
+      cvp.publish();  
+      cvp.publish();
    }
 }

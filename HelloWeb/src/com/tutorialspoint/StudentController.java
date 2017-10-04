4fb8422af106e75deb5e9e1a4de00295be34da48
Mathieu Seiwert <seiwert.mathieu@gmail.com>
2017-10-04 22:33:14 +0200

    Gestion exception

--- HelloWeb/src/com/tutorialspoint/StudentController.java
+++ HelloWeb/src/com/tutorialspoint/StudentController.java
@@ -1,11 +1,11 @@
 package com.tutorialspoint;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.ui.ModelMap;
 
 @Controller
 public class StudentController {
@@ -15,10 +15,20 @@ public class StudentController {
     }
 
     @RequestMapping( value = "/addStudent", method = RequestMethod.POST )
-    public String addStudent(Student student,ModelMap model ) {
+    @ExceptionHandler({SpringException.class})
+    public String addStudent(Student student,ModelMap model ) throws Exception {
         model.addAttribute( "name", student.getlalala() );
-        model.addAttribute( "age", student.getAge() );
-        model.addAttribute( "id", student.getId() );
+        if( student.getAge() < 10 ){
+            throw new SpringException("Given age is too low");
+         } else {
+            model.addAttribute("age", student.getAge());
+         }
+        if( student.getId() < 5 ){
+            throw new Exception("ID trop faible");
+         } else {
+             model.addAttribute( "id", student.getId() );
+         }
+       
         model.addAttribute( "student", student );
 
         return "resultTest";

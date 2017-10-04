0bc79ff360bcfb47101ba398e33956fa4bd844f7
Mathieu Seiwert <seiwert.mathieu@gmail.com>
2017-10-04 11:09:42 +0200

    Test de BeanPostProcessor

---
+++ HelloSpring/src/com/mathseiw/HelloWorld2.java
@@ -0,0 +1,10 @@
+package com.mathseiw;
+
+public class HelloWorld2 extends HelloWorld{
+    public void init(){
+       System.out.println("HelloWorld2 init.");
+    }
+    public void destroy() {
+       System.out.println("HelloWorld2 destroy.");
+    }
+ }
